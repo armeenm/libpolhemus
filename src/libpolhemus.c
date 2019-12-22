423d233bb27041d75c8ff1ce5d0d443e5f196c01
Armeen Mahdian <mahdianarmeen@gmail.com>
2019-12-22 17:01:29 -0600

    Setup directory structure

---
+++ src/libpolhemus.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int test(const char* str) {
+    puts(str);
+    return 0;
+}
---
+++ test/test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#include "libpolhemus.h"
+
+int main(void) {
+    test("Hello world");
+    puts("Hello world");
+
+    return 0;
+}
