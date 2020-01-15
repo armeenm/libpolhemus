5c0fe5ef94a5518fb7c0f7b4efd23856190f9d1d
Armeen Mahdian <mahdianarmeen@gmail.com>
2020-01-15 12:10:41 -0600

    Attempt to deal with cv-qualification

--- src/capi.h
+++ src/capi.h
@@ -7,18 +7,35 @@ extern "C" {
 
 #include <type_traits>
 
+/* Macro to ease checking and casting in conv
+ * res##c pasting is to have unique variable names to avoid shadowing warnings
+ */
+#define PLHM_CONV_CHK_(cxx, c)                                               \
+    if constexpr (auto res##c = multi_magic_cast<cxx, libpolhemus_##c>(val); \
+                  !std::is_null_pointer_v<decltype(res##c)>)                 \
+    return res##c
+
+/* Workaround for static_assert(false) */
+template <typename...>
+constexpr std::false_type always_false{};
+
 template <typename T, typename U>
 inline constexpr bool data_equal() {
-    return std::is_same_v<std::remove_pointer_t<std::decay_t<T>>, U>;
+    return std::is_same_v<
+        std::remove_pointer_t<std::decay_t<std::remove_const_t<T>>>, U>;
 }
 
 /* Cast according to pointerness */
 template <typename Target, typename Input>
 constexpr auto magic_cast(Input val) {
+    using FinalTarget =
+        typename std::conditional_t<std::is_const_v<Input>,
+                                    std::add_const_t<Target>, Target>;
+
     if constexpr (std::is_pointer_v<Input>)
-        return reinterpret_cast<std::add_pointer_t<Target>>(val);
+        return reinterpret_cast<std::add_pointer_t<FinalTarget>>(val);
     else
-        return static_cast<Target>(val);
+        return static_cast<FinalTarget>(val);
 }
 
 /* Call inverted magic_cast based on which Target the Input is */
@@ -31,18 +48,6 @@ constexpr auto multi_magic_cast([[maybe_unused]] Input val) {
     else
         return nullptr;
 }
-
-/* Macro to ease checking and casting in conv
- * res##c pasting is to have unique variable names to avoid shadowing warnings
- */
-#define PLHM_CONV_CHK_(cxx, c)                                               \
-    if constexpr (auto res##c = multi_magic_cast<cxx, libpolhemus_##c>(val); \
-                  !std::is_null_pointer_v<decltype(res##c)>)                 \
-    return res##c
-
-/* Workaround for static_assert(false) */
-template <typename...>
-constexpr std::false_type always_false{};
 
 /* Automagically convert C API types to C++ API types and vice-versa */
 template <typename T>
